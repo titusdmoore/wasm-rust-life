14e5e204255c185f1e50d721430e40775fdd91a2
titusdmoore <titusdmoore@gmail.com>
2024-01-11 23:26:41 -0500

    Added testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 mod utils;
 
-use std::cmp;
 use std::convert::TryInto;
 
 use wasm_bindgen::prelude::*;
@@ -31,29 +30,90 @@ impl Universe {
         // 0 0 0
         // 0 0 0
         // 0 0 0
-        for row_iter in row - 1..=row + 1 {
-            for column_iter in column - 1..=column + 1 {
-                if row_iter == row && column_iter == column {
+        for row_iter in row as i64 - 1..=row as i64 + 1 {
+            for column_iter in column as i64 - 1..=column as i64 + 1 {
+                if row_iter == row as i64 && column_iter == column as i64 {
                     continue;
                 }
 
-                let mut row_iter_index = row_iter % self.height;
-                let mut column_iter_index = column_iter % self.width;
+                let mut row_iter_index = row_iter % self.height as i64;
+                let mut column_iter_index = column_iter % self.width as i64;
 
                 if row_iter < 0 {
-                    row_iter_index = self.height + row_iter;
+                    row_iter_index = self.height as i64 + row_iter;
                 }
 
                 if column_iter < 0 {
-                    column_iter_index = self.width + column_iter;
+                    column_iter_index = self.width as i64 + column_iter;
                 }
 
-                if self.cells[self.get_index(row_iter_index, column_iter_index)] == Cell::Alive {
+                if self.cells[self.get_index(row_iter_index as u32, column_iter_index as u32)] == Cell::Alive {
                     live_count += 1;
                 }
             }
         }
 
         live_count
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_index() {
+        let universe = Universe {
+            width: 3,
+            height: 3,
+            cells: vec![Cell::Dead; 9],
+        };
+
+        assert_eq!(universe.get_index(0, 0), 0);
+        assert_eq!(universe.get_index(0, 1), 1);
+        assert_eq!(universe.get_index(1, 0), 3);
+        assert_eq!(universe.get_index(1, 1), 4);
+    }
+
+    #[test]
+    fn test_get_cell_live_neighbors() {
+        let mut universe = Universe {
+            width: 3,
+            height: 3,
+            cells: vec![Cell::Dead; 9],
+        };
+
+        let mut index = universe.get_index(0, 0);
+        universe.cells[index] = Cell::Alive;
+        index = universe.get_index(0, 1);
+        universe.cells[index] = Cell::Alive;
+        index = universe.get_index(0, 2);
+        universe.cells[index] = Cell::Alive;
+        index = universe.get_index(1, 0);
+        universe.cells[index] = Cell::Alive;
+        index = universe.get_index(1, 1);
+        universe.cells[index] = Cell::Alive;
+        index = universe.get_index(1, 2);
+        universe.cells[index] = Cell::Alive;
+        index = universe.get_index(2, 0);
+        universe.cells[index] = Cell::Alive;
+        index = universe.get_index(2, 1);
+        universe.cells[index] = Cell::Alive;
+        index = universe.get_index(2, 2);
+        universe.cells[index] = Cell::Alive;
+
+        // 1 1 1
+        // 1 1 1
+        // 1 1 1
+
+        assert_eq!(universe.get_cell_live_neighbors(0, 0), 8);
+        assert_eq!(universe.get_cell_live_neighbors(0, 1), 8);
+        assert_eq!(universe.get_cell_live_neighbors(0, 2), 8);
+        assert_eq!(universe.get_cell_live_neighbors(1, 0), 8);
+        assert_eq!(universe.get_cell_live_neighbors(1, 1), 8);
+        assert_eq!(universe.get_cell_live_neighbors(1, 2), 8);
+        assert_eq!(universe.get_cell_live_neighbors(2, 0), 8);
+        assert_eq!(universe.get_cell_live_neighbors(2, 1), 8);
+        assert_eq!(universe.get_cell_live_neighbors(2, 2), 8);
     }
 }
